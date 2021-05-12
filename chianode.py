b937ea003c6c8950a517a31757762320ca772561
Natasha Upchurch <TashaSkyUp@gmail.com>
2021-05-12 00:05:11 -0400

    fix

--- chianode.py
+++ chianode.py
@@ -23,13 +23,12 @@ def index():
 
 
 @app.route('/update')
-def restart():
+def update():
     os.system("cd /home/chianode/ChiaNodeService && git pull")
-
+    return ("hopeing it works")
 
 @app.route('/restart')
 def restart():
-
     os.system("sudo systemctl restart chianode")
     #os.system("sudo touch /home/chianode/ChiaNodeService/here.touch")
     #os.system("sudo chmod +777 /home/chianode/ChiaNodeService/here.touch")
