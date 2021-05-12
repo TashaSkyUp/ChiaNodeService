994731ca92331044f17fcc70eb5221335b1ba324
Natasha Upchurch <TashaSkyUp@gmail.com>
2021-05-12 01:39:08 -0400

    install and service creation merged

--- chianode.sh
+++
@@ -1,7 +0,0 @@
-sudo cp chianode.service /etc/systemd/system/
-systemctl daemon-reload
-systemctl start chianode
-systemctl enable chianode
-
-export FLASK_APP=chianode.py
-flask run
--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
-python3 -m venv /venv &&
+#environment
+python3 -m venv ./venv &&
 source venv/bin/activate &&
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt &&
+
+#service
+sudo cp chianode.service /etc/systemd/system/ &&
+systemctl daemon-reload &&
+systemctl start chianode &&
+systemctl enable chianode &&
