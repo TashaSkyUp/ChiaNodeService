534aa93d58c76b08140d4e4ec1b88cb731381933
Natasha Upchurch <TashaSkyUp@gmail.com>
2021-05-15 18:11:58 -0400

    addded raid array snippit

--- partition.sh
+++ partition.sh
@@ -9,25 +9,26 @@ do
 done
 ########################
 echo "Partioning"
-sudo sfdisk /dev/sda -a <<EOF
-,500115871
-,
-write
-EOF
 
 for value in b c d e f g h
 do
+sudo parted --script /dev/sd$value mklabel GPT
 sudo parted  /dev/sd$value rm 1
 sudo parted  /dev/sd$value rm 2
 sudo parted  /dev/sd$value rm 3
 sudo parted  /dev/sd$value rm 4
+done
 
-sudo parted  /dev/sd$value mkpart primary 500119552s 4TB
-sudo parted  /dev/sd$value mkpart primary 2048s  500119551
-sudo parted  /dev/sd$value mkpart primary 500119552s 4TB
-sudo parted  /dev/sd$value mkpart primary 500119552s 2TB
+for value in b c d e f g h
+do
+sudo parted --script /dev/sd$value mkpart primary 2048s  500119551s
+sudo parted --script /dev/sd$value mkpart primary 500119552s 4TB
+sudo parted --script /dev/sd$value mkpart primary 500119552s 2TB
+done
 
 ##########################
+for value in b c d e f g h
+do
 echo "Formatting"
 sudo partprobe /dev/sd$value
 sudo mkfs.xfs /dev/sd$value$[1] -f -m crc=0 -i maxpct=1 -l size=853b
@@ -58,3 +59,6 @@ do
     sudo chmod +777 /plot$i
 
 done
+
+sudo mdadm --create /dev/md0 -l 0 -n 2 /dev/sda5 /dev/sdb2
+sudo mdadm --create /dev/md1 -l 5 -n 7 /dev/md0 /dev/sdc2 /dev/sdd2 /dev/sde2 /dev/sdf2 /dev/sdg2 /dev/sdh2
