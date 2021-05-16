a85fb6c0975d6ed34adc889ab50ec3a5a9bbe644
Natasha Upchurch <TashaSkyUp@gmail.com>
2021-05-16 01:40:32 -0400

    fixed bug that caused lanes to die when new lanes started

--- lane.sh
+++ lane.sh
@@ -2,6 +2,6 @@
 chiaexec="/usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon/chia"
 
 while sleep 1; do sleep 10s &&
-kill $(ps -ef |grep /plot$3| grep chia |awk '{print $3}')
+kill $(ps -ef |grep "/plot$3"| grep chia |awk '{print $3}')
 rm /plot$3/*
 nohup $chiaexec plots create -k 32 -n 1 -b $1 -r $2 -t /plot$3 -d /farm$4 --override-k > lane$3.txt; done &
--- mount_nvme_3x3x2.sh
+++ mount_nvme_3x3x2.sh
@@ -7,3 +7,13 @@ sudo mount /dev/nvme0n1p3 /plot6
 sudo mount /dev/nvme1n1p1 /plot7
 sudo mount /dev/nvme1n1p2 /plot8
 sudo mount /dev/nvme1n1p3 /plot9
+
+# /dev/nvme2n1p1 /plot1
+# /dev/nvme2n1p2 /plot2
+# /dev/nvme2n1p3 /plot3
+# /dev/nvme0n1p1 /plot4
+# /dev/nvme0n1p2 /plot5
+# /dev/nvme0n1p3 /plot6
+# /dev/nvme1n1p1 /plot7
+# /dev/nvme1n1p2 /plot8
+# /dev/nvme1n1p3 /plot9
