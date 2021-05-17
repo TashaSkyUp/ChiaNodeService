7fd1112679d52da378a02999395c3b40eb75917b
Natasha Upchurch <TashaSkyUp@gmail.com>
2021-05-17 05:49:54 -0400

    more shell utils

--- benchmark_plots.sh
+++ benchmark_plots.sh
@@ -4,6 +4,7 @@ p=13
 
 for i in `seq 12 20`
 do
-dd if=/dev/zero of=/plot$((i))/tmp.tmp bs=$((1))G count=1 oflag=dsync
-dd if=/dev/zero of=/farm$((i))/tmp.tmp bs=$((1))G count=1 oflag=dsync
+  echo $i
+  dd if=/dev/zero of=/plot$((i))/tmp.tmp bs=$((1))G count=1 oflag=dsync
+  dd if=/dev/zero of=/farm$((i))/tmp.tmp bs=$((1))G count=1 oflag=dsync
 done
