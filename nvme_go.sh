6e13293ffef793c9e69dd3e7e67dd6d165ddc67a
Natasha Upchurch <TashaSkyUp@gmail.com>
2021-05-17 23:21:48 -0400

    more shell utils

--- nvme_go.sh
+++ nvme_go.sh
@@ -4,5 +4,9 @@ rm /plot0/*
 
 for i in 12 13 14 15 16 17 18 19 12 13 14 15
 do
-  while sleep 1; do nohup $chiaexec plots create -k 32 -n 1 -b $1 -r $2 -t /plot0 -d /farm$i --override-k > laneNvme$i.txt; done &
+  while sleep 1
+  do
+    nohup $chiaexec plots create -k 32 -n 1 -b $1 -r $2 -t /plot0 -d /farm$i --override-k > laneNvme$i.txt;
+    sleep 30m
+  done &
 done
