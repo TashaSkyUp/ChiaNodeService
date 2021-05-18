3587585912267509b908e67ba623d305ca073395
Natasha Upchurch <TashaSkyUp@gmail.com>
2021-05-18 00:26:08 -0400

    more shell utils

--- nvme_go.sh
+++ nvme_go.sh
@@ -2,11 +2,10 @@ chiaexec="/usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon/chia"
 
 rm /plot0/*
 
-for i in 12 13 14 15 16 17 18 19 12 13 14 15
+while sleep 1
 do
-  while sleep 1
+  for i in 12 13 14 15 16 17 18 19 12 13 14 15
   do
-    $chiaexec plots create -k 32 -n 1 -b $1 -r $2 -t /plot0 -d /farm$i --override-k > laneNvme$i.txt &
-    sleep 30m
+    $chiaexec plots create -k 32 -n 1 -b $1 -r $2 -t /plot0 -d /farm$i --override-k > laneNvme$i.txt & sleep 30m
   done
 done &
