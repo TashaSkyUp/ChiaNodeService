054ed5f7f559c5d4477449f49af9eceea2af6a15
Natasha Upchurch <TashaSkyUp@gmail.com>
2021-05-18 14:56:24 -0400

    more shell utils

--- benchmark_plots.sh
+++ benchmark_plots.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
-p=13
-
-for i in `seq 12 20`
+for i in `seq $1 $2`
 do
   echo $i
-  dd if=/dev/zero of=/plot$((i))/tmp.tmp bs=$((1))G count=1 oflag=dsync
-  dd if=/dev/zero of=/farm$((i))/tmp.tmp bs=$((1))G count=1 oflag=dsync
+  testfile=/plot$((i))/tmp.tmp
+  dd if=/dev/zero of=/$testfile$((1)) bs=$((1))G count=1 oflag=dsync &
+  dd if=/dev/zero of=/$testfile$((1)) bs=$((1))G count=1 oflag=dsync &
+  dd if=/dev/zero of=/$testfile$((1)) bs=$((1))G count=1 oflag=dsync
+  rm $testfile$((1))
+  rm $testfile$((2))
+  rm $testfile$((3))
+
 done
