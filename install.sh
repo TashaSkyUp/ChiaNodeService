de960235646d7072a4905d33e3201d4325877dc4
Natasha Upchurch <TashaSkyUp@gmail.com>
2021-05-19 18:33:38 -0400

    attempting venv activation fix

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ apt-get install python3-venv
 echo "creating venv" &&
 python3 -m venv ./venv &&
 echo "activating venv" &&
-source venv/bin/activate &&
+source ./venv/bin/activate &&
 echo "install requirements" &&
 pip3 install -r requirements.txt &&
 
