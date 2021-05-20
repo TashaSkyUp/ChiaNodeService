4fcf9ee226b1e5a6b1f906c5c0e4f7fce5b5c46f
Natasha Upchurch <TashaSkyUp@gmail.com>
2021-05-20 14:52:19 -0400

    feature: ability to change plot / farm prefix

--- lane.sh
+++ lane.sh
@@ -1,7 +1,32 @@
-#memory, threads ,/plot num ,/farm num
+#!/bin/bash
+if [ $# -eq 0 ]
+  then
+    echo "memory, threads ,/plot num ,/farm num, plot prefix, farm prefex"
+    exit
+fi
+
 chiaexec="/usr/lib/chia-blockchain/resources/app.asar.unpacked/daemon/chia"
+f_pfx=/farm
+
+if [ -z $5 ]
+  then
+    p_pfx=/plot
+  else
+    p_pfx=$5
+  fi
+echo $p_pfx
+
+if [ -z $6 ]
+  then
+    f_pfx=/farm
+  else
+    f_pfx=$6
+  fi
+echo $f_pfx
+
+exit 1
 
 while sleep 1; do sleep 10s &&
 kill $(ps -ef |grep "/plot$3 "| grep chia |awk '{print $3}')
-rm /plot$3/*
+rm /plot$3/*.tmp
 nohup $chiaexec plots create -k 32 -n 1 -b $1 -r $2 -t /plot$3 -d /farm$4 --override-k > lane$3.txt; done &
