4b4bed59fe78f696764c742e248a391d63dc0fe2
Natasha Upchurch <TashaSkyUp@gmail.com>
2021-05-24 20:03:22 -0400

    more utils

---
+++ hardware_utils.py
@@ -0,0 +1,67 @@
+#!/bin/python3
+import subprocess
+import re
+
+
+def get_disk_info():
+    cmd = "sudo hwinfo --disk".split(" ")
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE)
+    result = proc.communicate()
+    result = result[0].decode("utf8").splitlines()
+    out = []
+    for line in result:
+        tmp = line.split(":")
+        if len(line) > 2:
+            if len(tmp) == 1:
+                # print ("one",line)
+                tmp = line.split(" at ")
+                # print("two",tmp)
+                line = tmp[0] + ":" + tmp[1]
+
+            elif len(tmp) >= 3:
+                # print (tmp)
+                line = tmp[0] + ":" + " ".join(tmp[1:])
+            else:
+                pass
+                # print(line)
+                # print(tmp)
+
+            out = out + [line]
+    out_dict = {}
+    for line in out:
+        tmp = line.strip().split(":")
+        try:
+            num = int(tmp[0])
+            out_dict[line] = {}
+            cur_key = line
+        except:
+            out_dict[cur_key][tmp[0]] = tmp[1]
+
+    # result = re.match('[0-9a-zA-Z:\- ]+',result )
+    # result = re.match('[]', result)
+
+    # print('\n'.join(result))
+    # print ('\n'.join(out))
+    for data in out_dict.items():
+        #print("----->", data[0], "<-----")
+        for data2 in data[1].items():
+            pass
+            #print(data2[0], data2[1])
+
+    out_dict2 = {}
+    for data in out_dict.items():
+        key = out_dict[data[0]]["Device File"]
+        print(key)
+
+        out_dict2[key] = {}
+        data_in = out_dict[data[0]]
+        print('\n'.join(data_in))
+
+        if "Model" in data_in.keys():
+            out_dict2[key]["Model"]=data_in["Model"]
+        if "Vendor" in data_in.keys():
+            out_dict2[key]["Vendor"] = data_in["Vendor"]
+
+
+    print(out_dict2)
+get_disk_info()
