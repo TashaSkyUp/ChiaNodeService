3e7cf709e180c7e6d2c3089a0cd50785858847fd
Natasha Upchurch <TashaSkyUp@gmail.com>
2021-05-25 01:41:28 -0400

    fix

--- view_reports.sh
+++ view_reports.sh
@@ -2,7 +2,7 @@ for file in $(find "." -name "lane*.txt" -size +205900c);
  do
    plotnum=$(echo $file |grep -o '[0-9]\+'| head -n 1)
    echo $plotnum
-   df /plot$plotnum | tail -n 1 | grep -o '[0-9a-z]\+ '
+   df /plot$plotnum | tail -n 1 | grep -o '[0-9a-z]\+ ' | head -n 1
    #plot=$(cat $file |head -n 10| grep "/plot"|grep -o '/plot[0-9]\+': head -n 1)
    #echo $plot
    cat $file |head -n 10| grep "threads"
