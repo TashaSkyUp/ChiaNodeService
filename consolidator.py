8b9007b9bae027e09a6c96165dea885c6b295b7e
Natasha Upchurch <TashaSkyUp@gmail.com>
2021-05-26 04:18:43 -0400

    limit source file selection to files larger than 108GB

--- consolidator.py
+++ consolidator.py
@@ -37,13 +37,16 @@ def find_fullist_farm(info: dict):
     arr = np.array(l)
     return list(info.items())[arr.argmin()][0]
 
-def find_oldist_file_in_dir(path):
+def find_oldist_file_in_dir(path,minsize = 108000000000):
     #print(path)
     list_of_files = os.listdir(path)
     #print(list_of_files)
     full_path = [path+"/"+str(x) for x in list_of_files if x[-4:] == "plot"]
+    full_path = [x for x in full_path if os.path.getsize(x) >= minsize]
+
     #print(full_path)
     oldest_file = min(full_path, key=os.path.getmtime)
+
     return oldest_file
 
 
