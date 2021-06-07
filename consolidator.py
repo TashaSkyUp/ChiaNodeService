8ce9b80add811eb1f56bdca2f7765a3f676a9013
Natasha Upchurch <tashaskyup@gmail.com>
2021-06-06 20:36:30 -0400

    fix for not detecting full export

--- consolidator.py
+++ consolidator.py
@@ -49,9 +49,10 @@ def find_oldist_file_in_dir(path, minsize=108000000000):
     # print(path)
     list_of_files = os.listdir(path)
     # print(list_of_files)
+    #full_path = [path + "/" + str(x) for x in list_of_files if x[-4:] == "plot"]
     full_path = [path + "/" + str(x) for x in list_of_files if x[-4:] == "plot"]
     full_path = [x for x in full_path if os.path.getsize(x) >= minsize]
-
+    print (path)
     print(full_path)
     oldest_file = min(full_path, key=os.path.getmtime)
 
